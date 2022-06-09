89720a93d9bc39294f27d4709581bb1c2d25256f
Fabio <fabio@itexto.com.br>
2022-06-09 09:13:08 -0300

    aula 1 modulo 2

---
+++ modulo 2/capitulo-2/livraria/src/br/com/caelum/livraria/bean/AutorBean.java
@@ -0,0 +1,63 @@
+package br.com.caelum.livraria.bean;
+
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import br.com.caelum.livraria.dao.DAO;
+import br.com.caelum.livraria.modelo.Autor;
+
+@ManagedBean
+@ViewScoped
+public class AutorBean {
+
+	private Autor autor = new Autor();
+	
+	private Integer autorId;
+	
+	
+
+	public Integer getAutorId() {
+		return autorId;
+	}
+
+	public void setAutorId(Integer autorId) {
+		this.autorId = autorId;
+	}
+	
+	public void carregarAutorPelaId() {
+		this.autor = new DAO<Autor>(Autor.class).buscaPorId(autorId);
+	}
+
+	public String gravar() {
+		System.out.println("Gravando autor " + this.autor.getNome());
+
+		if(this.autor.getId() == null) {
+			new DAO<Autor>(Autor.class).adiciona(this.autor);
+		} else {
+			new DAO<Autor>(Autor.class).atualiza(this.autor);
+		}
+
+		this.autor = new Autor();
+
+		return "livro?faces-redirect=true";
+	}
+	
+	public void remover(Autor autor) {
+		System.out.println("Removendo autor " + autor.getNome());
+		new DAO<Autor>(Autor.class).remove(autor);
+	}
+	
+	public List<Autor> getAutores() {
+		return new DAO<Autor>(Autor.class).listaTodos();
+	}
+	
+	public Autor getAutor() {
+		return autor;
+	}
+
+	public void setAutor(Autor autor) {
+		this.autor = autor;
+	}
+}
---
+++ modulo 2/capitulo-2/livraria/src/br/com/caelum/livraria/bean/LoginBean.java
@@ -0,0 +1,42 @@
+package br.com.caelum.livraria.bean;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+import br.com.caelum.livraria.dao.UsuarioDao;
+import br.com.caelum.livraria.modelo.Usuario;
+
+@ManagedBean
+@ViewScoped
+public class LoginBean {
+
+	private Usuario usuario = new Usuario();
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	
+	public String efetuaLogin() {
+		System.out.println("fazendo login do usuario " + this.usuario.getEmail());
+		
+		FacesContext context = FacesContext.getCurrentInstance();
+		boolean existe = new UsuarioDao().existe(this.usuario);
+		if(existe ) {
+			context.getExternalContext().getSessionMap().put("usuarioLogado", this.usuario);
+			return "livro?faces-redirect=true";
+		}
+		
+		context.getExternalContext().getFlash().setKeepMessages(true);
+		context.addMessage(null, new FacesMessage("Usuário não encontrado"));
+		
+		return "login?faces-redirect=true";
+	}
+	
+	public String deslogar() {
+		FacesContext context = FacesContext.getCurrentInstance();
+		context.getExternalContext().getSessionMap().remove("usuarioLogado");
+		return "login?faces-redirect=true";
+	}
+}
